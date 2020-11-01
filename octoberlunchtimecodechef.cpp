669ba7686b45dc694076f6111d7759425135ad1a
Mohan kumar <73782028+mohan8645@users.noreply.github.com>
2020-11-02 01:17:52 +0530

    Update and rename andorcodchef.cpp to octoberlunchtimecodechef.cpp

--- octoberlunchtimecodechef.cpp
+++ octoberlunchtimecodechef.cpp
@@ -1,3 +1,6 @@
+//Given an integer x, find two non-negative integers a and b such that (a∧b)+(a∨b)=x, where ∧ is the bitwise AND operation and ∨ is the bitwise OR operation.
+
+
 #include<bits/stdc++.h>
 #define fastio ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 #define ll long long int
